eac1cfcc792c37a101484bc87494d9d70bf821f8
VitalySkupov <skupovvv@gmail.com>
2019-04-24 14:17:43 +0300

    Удалили ненужеый код

---
+++ cut_cake.py
@@ -0,0 +1,10 @@
+def cut_cake(parts):
+    try:
+        return 1 / int(parts)
+    except (ZeroDivisionError, TypeError, ValueError):
+        return 'Вы что, с ума посходили?'
+
+cake = cut_cake([1, 2, 5])
+print(cake)
+
+
